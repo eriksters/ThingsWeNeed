a62f403cd0870f93dad3e552c94b396e1c59e1a3
Krzysztof Ziach <krzysiek.ziach@gmail.com>
2019-11-18 16:26:37 +0100

    Modal pop-up, buttons' even listeners

--- ThingsWeNeed/Scripts/ViewsScripts/ThingsListScripts.js
+++ ThingsWeNeed/Scripts/ViewsScripts/ThingsListScripts.js
@@ -1,91 +1,103 @@
 ﻿$(function () {
 
-        var modal = document.getElementById("myModal");
+    //Get the buttons that open the modal
+    var btn = document.getElementsByClassName("options-button");
 
-        //Get the button that opens the modal
-        var btn = document.getElementsByClassName("options-button");
+    //Get the button that closes the modal
+    var modalCloseButton = document.getElementById("modal-close-button");
 
-        var modalCloseButton = document.getElementById("modal-close-button")
+    //Get the modal 
+    var modal = document.getElementById("myModal");
 
-        //When the user clicks on the button, open the modal
-        $(btn).click(function () {
-            modal.style.display = "block";
-        });
+    //Override click function for buttons opening the modal
+    $(btn).click(function () {
+        openModal();
+    });
 
-        $(modalCloseButton).click(function () {
-            modal.style.display = "none"
-        });
+    //Open modal on a button click
+    function openModal() {
+        name = event.currentTarget.name; //name equivalent to Thing's name
+        header = document.getElementById("modal-header");
+        header.innerText = name;
+        modal.style.display = "block"
+    }
+
+    //Close the modal when "Close" button is clicked
+    $(modalCloseButton).click(function () {
+        modal.style.display = "none"
+    });
 
 
-        //When the user clicks anywhere outside of the modal, close it
-        $(window).click(function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        });
-
-        $(document).ready(function () {
+    //When the user clicks anywhere outside of the modal, close it
+    $(window).click(function (event) {
+        if (event.target == modal) {
             modal.style.display = "none";
-        });
+        }
+    });
 
-        //  Map that stores the 'buy state' of things
-        //  The key is the thing's Id, value is a boolean. TRUE = buying, FALSE = not buying
-        var buyMap = new Map();
+    //When document is ready, close modal
+    $(document).ready(function () {
+        modal.style.display = "none"
+    });
 
-        //  When a buy button is clicked, add the thing to the buyMap, with a value of TRUE
-        $("button.buy_button").click(function () {
-            $(this).hide();
-            $(this).parent().children("div.btn_stopper")
-                .show();
-            buyMap.set($(this).closest("tr").attr("id"), true);
-        });
+    //  Map that stores the 'buy state' of things
+    //  The key is the thing's Id, value is a boolean. TRUE = buying, FALSE = not buying
+    var buyMap = new Map();
 
-        //  When a cancel button is clicked, set the thing's buy state to FALSE
-        $("input.cancel_button").click(function () {
-            var $divElement = $(this).parent().parent();
-            $divElement.parent().children("button.buy_button")
-                .show();
-            $divElement.hide();
-            buyMap.set($(this).closest("tr").attr("id"), false);
-        });
+    //  When a buy button is clicked, add the thing to the buyMap, with a value of TRUE
+    $("button.buy_button").click(function () {
+        $(this).hide();
+        $(this).parent().children("div.btn_stopper")
+            .show();
+        buyMap.set($(this).closest("tr").attr("id"), true);
+    });
 
-        //  Save the purchase
-        $("button#save_button").click(function () {
+    //  When a cancel button is clicked, set the thing's buy state to FALSE
+    $("input.cancel_button").click(function () {
+        var $divElement = $(this).parent().parent();
+        $divElement.parent().children("button.buy_button")
+            .show();
+        $divElement.hide();
+        buyMap.set($(this).closest("tr").attr("id"), false);
+    });
 
-            //  List that stores the items that a User is buying
-            var buyList = new Array();
-            var $theButton = $(this);
+    //  Save the purchase
+    $("button#save_button").click(function () {
 
-            //  Loop through the buyMap and if a thing is to be bought, add it to the needsList as an Object { ThingId: -id-, ThingPrice: -price- }
-            buyMap.forEach(function (value, key) {
-                if (value === true) {
-                    var idString = "#" + key;
-                    var thingPrice = $(document).find(idString).find("input.price_input")[0].value;
-                    buyList.push({
-                        ThingId: idString.slice(1, idString.length),
-                        ThingPrice: new Number(thingPrice)
-                    });
-                }
-            });
+        //  List that stores the items that a User is buying
+        var buyList = new Array();
+        var $theButton = $(this);
 
-            //  Get the request URL from the HTML tag
-            var urlStr = $theButton.data('request-url');
-
-            //  Send the POST request to the server as a JSON
-            //  JSON Structure: { purchases: [{ThingId: -id1-, ThingPrice: -price1-}..]}
-            if (buyList.length > 0) {
-                $.ajax({
-                    url: urlStr,
-                    dataType: 'json',
-                    type: 'POST',
-                    data: JSON.stringify({ purchases: buyList }),
-                    contentType: 'application/json',
-                    success: function () {
-                        return data;
-                    }
+        //  Loop through the buyMap and if a thing is to be bought, add it to the needsList as an Object { ThingId: -id-, ThingPrice: -price- }
+        buyMap.forEach(function (value, key) {
+            if (value === true) {
+                var idString = "#" + key;
+                var thingPrice = $(document).find(idString).find("input.price_input")[0].value;
+                buyList.push({
+                    ThingId: idString.slice(1, idString.length),
+                    ThingPrice: new Number(thingPrice)
                 });
             }
         });
+
+        //  Get the request URL from the HTML tag
+        var urlStr = $theButton.data('request-url');
+
+        //  Send the POST request to the server as a JSON
+        //  JSON Structure: { purchases: [{ThingId: -id1-, ThingPrice: -price1-}..]}
+        if (buyList.length > 0) {
+            $.ajax({
+                url: urlStr,
+                dataType: 'json',
+                type: 'POST',
+                data: JSON.stringify({ purchases: buyList }),
+                contentType: 'application/json',
+                success: function () {
+                    return data;
+                }
+            });
+        }
+    });
 });
 
 
